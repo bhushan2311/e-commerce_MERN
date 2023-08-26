ab53c218e503dd797272340c4e88718d0f90b929
bhushan2311 <bambhore350@gmail.com>
2023-08-26 21:49:12 +0530

    Login and Signup page added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 // import { ProductList } from './features/product-list/ProductList';
 import './App.css';
 import Home from './pages/Home';
-
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      {/* <LoginPage></LoginPage> */}
+      <SignupPage></SignupPage>
     </div>
   );
 }
