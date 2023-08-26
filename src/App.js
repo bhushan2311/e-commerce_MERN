9ecc5997de1b3b9d61c17850d9dee21050e8a682
bhushan2311 <bambhore350@gmail.com>
2023-08-26 21:17:11 +0530

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { ProductList } from './features/product-list/ProductList';
+// import { ProductList } from './features/product-list/ProductList';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <ProductList/>
+      <Home/>
     </div>
   );
 }
